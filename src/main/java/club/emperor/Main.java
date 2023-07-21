ef3275ebd6f71398b5a3e66b9dbed8d703418b84
EmperorWS <1970912595@qq.com>
2023-07-21 22:23:07 +0800

    [ADD]添加User-Agent解析测试类

--- src/main/java/club/emperor/Main.java
+++ src/main/java/club/emperor/Main.java
@@ -1,8 +1,5 @@
 package club.emperor;
 
-import net.sf.uadetector.ReadableUserAgent;
-import net.sf.uadetector.UserAgentStringParser;
-import net.sf.uadetector.service.UADetectorServiceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,9 +17,5 @@ public class Main {
     public static void main(String[] args) {
         /*Type type = new TypeToken<Response<String>>() {
         }.getType();*/
-        String userAgentString = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82";
-        UserAgentStringParser parser = UADetectorServiceFactory.getResourceModuleParser();
-        ReadableUserAgent userAgent = parser.parse(userAgentString);
-        logger.info("User Agent: " + userAgent);
     }
 }
